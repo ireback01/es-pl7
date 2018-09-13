74f077a37a667465765c77da4be48b9dc64d99cc
KinryuBarao <s.franciscobarao@gmail.com>
2018-09-13 16:34:52 +0100

    Login

--- projectPl7/settings.py
+++ projectPl7/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = "/"    
