80d64a3a5d88ffdb0b007653a7b5e322564b4fd0
KinryuBarao <s.franciscobarao@gmail.com>
2018-09-13 17:05:51 +0100

    Auto stash before merge of "master" and "origin/master"

--- projectPl7/settings.py
+++ projectPl7/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'feed.apps.FeedConfig',
     'login',
+    'feed.apps.FeedConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
