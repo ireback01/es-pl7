0bda81c8cbf3270cffab1b424b3f1723ec3e55d4
KinryuBarao <s.franciscobarao@gmail.com>
2018-09-14 19:52:23 +0100

    Register/Login changes
    Work in process

--- feed/views.py
+++ feed/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 def home(request):
     return render(request,'home/home.html')
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid(): 
+            form.save() #Save info to database -> create new user
+            username = form.cleaned_data['username'] #take username from form
+            password = form.cleaned_data['password1'] #take password from form
+            user = authenticate(username=username, password=password) #takes care of hashing and so on
+            messages.info(request, 'User created with Sucess!')
+            return redirect('login')
+
+    else:
+        form = UserCreationForm()
+    
+    
     context = {'form' : form } 
     return render(request,'registration/register.html',context)
 
+
+
+#Fazer mais campos na form (Email entre outros)
+#keep working on this
