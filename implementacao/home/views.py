b8ea77bc9edcf0e18b864f62b1584ca13f9e510c
KinryuBarao <s.franciscobarao@gmail.com>
2018-11-13 14:04:48 +0000

    new migrations

--- implementacao/home/views.py
+++ implementacao/home/views.py
@@ -7,7 +7,4 @@ from user.forms import TweetForm
 
 @login_required
 def home(request):
-	post_list = Post.objects.all()
-	profile = Profile.objects.get(id=request.user.id)
-	tweet_form = TweetForm()
-	return render(request,'home/index.html', context={'posts': post_list, 'profile': profile, 'tweet_form': tweet_form})
+	return
