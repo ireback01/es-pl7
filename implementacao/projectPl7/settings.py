1e084b328baf1a5ef09c89e86c685835557b3029
KinryuBarao <s.franciscobarao@gmail.com>
2018-12-10 14:11:39 +0000

    Auto stash before merge of "development" and "origin/development"
    Create Temp directory automatically

--- implementacao/projectPl7/settings.py
+++ implementacao/projectPl7/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#Create Temp directory for image upload
+if not os.path.exists("images/temp"):
+    os.makedirs("images/temp")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
